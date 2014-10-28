a80cb0dafcb4f3dd6dbcd83c24c58f0ddd155790
Nobusada <daniel.nobusada@gmail.com>
2014-10-28 01:52:05 -0200

    Salva imagens conforme os vértices de maior peso são removidos.

--- src/le_matriz_adjacencia.py
+++ src/le_matriz_adjacencia.py
@@ -1,60 +1,89 @@
-# Comandos em python para leitura de um grafo a partir de uma matriz de 
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Comandos em python para leitura de um grafo a partir de uma matriz de
 # adjacencia gravada em arquivo texto
 
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
+import platform
 
-dataset = 'sgb128'
-dataset_dist = '../dataset/' + dataset + '_dist.txt'
-dataset_name = '../dataset/' + dataset + '_name.txt'
+# Resolve o caminho absoluto
+abs_path = os.path.abspath("sgb128_dist.txt")
+pre_path = abs_path[:-(len("sgb128_dist.txt") + 4)]
 
-a = np.loadtxt(dataset_dist)
-labels = np.loadtxt(dataset_name, dtype=basestring)
+# Executa para cada dataset individual
+dataset_list = ['uk12', 'wg59', 'sgb128']
+for dataset in dataset_list:
 
-# Obtem as coordenadas em que o peso eh nao-nulo
-rows, cols= np.where(a > 0)
+    # Verifica o sistema operacional
+    if platform.system() == 'Linux':
+        dataset_dist = pre_path + 'dataset/' + dataset + '_dist.txt'
+        dataset_name = pre_path + 'dataset/' + dataset + '_name.txt'
+    elif platform.system() == 'Windows':
+        dataset_dist = pre_path + 'dataset\\' + dataset + '_dist.txt'
+        dataset_name = pre_path + 'dataset\\' + dataset + '_name.txt'
 
-# Obtem o peso de cada uma das arestas
-weight = [a[rows[i],cols[i]] for i in range(0, len(rows))]
+    a = np.loadtxt(dataset_dist)
+    labels = np.loadtxt(dataset_name, dtype=basestring, delimiter='\n')
 
-# Cria lista de arestas com e sem peso
-edges = zip(rows.tolist(), cols.tolist())
-edges_with_weight = zip(rows.tolist(), cols.tolist(), weight)
+    # Obtem as coordenadas em que o peso eh nao-nulo
+    rows, cols= np.where(a > 0)
 
-# Cria um grafo vazio usando NetworkX
-g = nx.Graph()
+    # Obtem o peso de cada uma das arestas
+    weight = [a[rows[i],cols[i]] for i in range(0, len(rows))]
 
-# Insere arestas (cria vertices automaticamente)
-# Para inserir os pesos usar um dicionario como parametro adicional
-g.add_edges_from(edges)
-#g.add_weighted_edges_from(edges_with_weight)
+    # Cria lista de arestas com e sem peso
+    edges_with_weight = zip(rows.tolist(), cols.tolist(), weight)
 
-# Linka os nomes com seus respectivos vertices
-nomes = dict(zip(g.nodes(), labels.tolist()))
+    # Cria um grafo vazio usando NetworkX
+    g = nx.Graph()
 
-# Mostra vertices e arestas
+    # Insere primeiro os vértices nomeados
+    set_edges = set(rows.tolist())
+    for i in range(0,len(set_edges)):
+        g.add_node(i, name=labels.tolist()[i])
 
-#print g.nodes()
-#print g.edges(data=True)
+    # Insere arestas
+    g.add_weighted_edges_from(edges_with_weight)
+    # Linka os nomes com seus respectivos vertices
 
-# Plota resultados em arquivo
+    # Executa o algoritmo de Prim para extrair a MST do grafo
+    edge_list = nx.generate_edgelist(g,data=['weight'])
+    mst = nx.kruskal_mst(g)
 
-pos = nx.circular_layout(g)
+    # Plota resultados, é necessário que seja salvo na mão.
+    # Opção de layout de grafo usado foi o Fruchterman & Reingold
 
-nx.draw_networkx_nodes(g, pos, node_size=70)
-nx.draw_networkx_edges(g, pos)
-nx.draw_networkx_labels(g, pos, nomes, font_size=10)
+    plt.figure(1, figsize=(15,15))		# Cria figura para desenhar grafo: 15 eh a dimensao da imagem
+    #nx.draw_spring(mst, node_size=350, font_size=10, edge_width=1, alpha=0.5, arrows=False, with_labels=True)
 
-plt.axis('off')
-plt.show()
+    # Coordenadas mantém as coordenadas dos vértices do grafo
+    coordinates = nx.spring_layout(mst)
+    # Salva a primeira imagem
+    nx.draw(mst, coordinates, node_size=350, font_size=10, edge_width=1, alpha=0.5, arrows=False, with_labels=True)
+    plt.savefig(dataset+'.png')		# Outros formatos: pdf, svg, ...
+    plt.show()
 
-#plt.figure(1, figsize=(15,15))		# Cria figura para desenhar grafo: 15 eh a dimensao da imagem
-#nx.draw_spring(g, node_size=120, font_size=3, edge_width=0.5, alpha=0.5, arrows=False)
-#plt.show()
-#plt.savefig('arquivo.png')		# Outros formatos: pdf, svg, ...
+    # Função que ordena inversamente pelo peso, por causa da função lambda utilizada
+    edges_by_weight = sorted([(i,j,mst.edge[i][j]['weight']) for i,j in mst.edges()], key=lambda a:a[2], reverse=True)
+    name_counter = 0
+    # Remoção das arestas com maior peso para a detecção de comunidades
+    # Press "e" to exit
 
+    print "'Enter' para remover uma aresta, 'e' para passar para o próximo grafo, ou sair do programa no último grafo"
+    while raw_input() != "e":
+        print "Removendo a aresta de maior peso!!!!!!!!!!!"
 
-# OBS: Para leitura de outros formatos ver funcoes
-# nx.read_dot, nx.read_edgelist, nx.read_weighted_edgelist, nx.read_gexf, nx.read_gml, nx.read_graphml,...
- 
+        # Remoção efetiva da aresta que possui o maior peso
+        mst.remove_edge(edges_by_weight[0][0], edges_by_weight[0][1])
+        edges_by_weight = edges_by_weight[1:]
+
+        # Salvar a imagem sem a aresta de maior peso
+        plt.figure(1, figsize=(15,15))
+        nx.draw(mst, coordinates, node_size=350, font_size=10, edge_width=1, alpha=0.5, arrows=False, with_labels=True)
+        plt.savefig(dataset + '_img' + str(name_counter) + '.png')		# Outros formatos: pdf, svg, ...
+        plt.show()
+        name_counter += 1
